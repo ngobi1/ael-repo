77da29c03329b488f46b1f43260f45e8f389a95f
Stim Ngobi <stimngobi@gmail.com>
2020-10-18 23:31:03 +0300

    php tags removed

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<div>Hello World</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AEL Home</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+    
+    <div>Hello World</div>
+
+</body>
+</html>
