cd951b56fc5bfa85f807b08ac3a175498e339be5
Stim Ngobi <stimngobi@gmail.com>
2020-10-26 21:59:55 +0300

    unfinished footer

--- index.php
+++ index.php
@@ -69,6 +69,77 @@
 
     </div>
 
+    <div id="wrapper">
+    
+        <div class="container">
+
+            <div class="row footer">
+
+                <div class="col-md-3">
+                    <h5>Abainemu Enterprises Limited</h5>
+
+                    <hr style="background-color: aliceblue;">
+
+                    <p>Lorem ipsum dolor sit amet consectetur 
+                        adipisicing elit. Quod minus officiis ea 
+                        possimus suscipit voluptate amet nobis 
+                        fugiat, itaque blanditiis maxime expedita, 
+                        magni quia quos veniam, nemo molestiae 
+                        aperiam modi.</p>
+                </div>
+                <div class="col-md-3">
+                    <h5>Quick Links</h5>
+
+                    <hr style="background-color: aliceblue;">
+
+                    <ul>
+                        <li>Services</li>
+                        <li>Products</li>
+                        <li>About US</li>
+                        <li>Contact US</li>
+                    </ul>  
+                </div>
+                <div class="col-md-3">
+                    <h5>News Feed</h5>
+
+                    <hr style="background-color: aliceblue;">
+
+                    <p>Lorem ipsum dolor sit amet consectetur 
+                        adipisicing elit. In reiciendis accusamus
+                        harum?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur 
+                        adipisicing elit. Voluptatibus, nostrum.
+                        provident magnam ab porro.</p>
+                </div>
+                <div class="col-md-3">
+                    <h5>Contact Us</h5>
+
+                    <hr style="background-color: aliceblue;">
+
+                    <h6>Abainemu Enterprises Ltd</h6>
+                    <h6>Sir Apollo Kaggwa Road</h6>
+                    <h6>P.O.Box #### Kampala</h6>
+                    <h6>Tel: +256 392 ******</h6>
+                    <h6>Email: info@abainemu.org</h6>
+                    <i class="fab fa-facebook" style="padding-right: 20px;"><a href="facebook"></a></i>
+                    <i class="fab fa-twitter" style="padding-right: 20px;"><a href="twitter"></a></i>
+                    <i class="fab fa-linkedin" style="padding-right: 20px;"><a href="linkedin"></a></i>
+                    <i class="fab fa-instagram" style="padding-right: 20px;"><a href="instagram"></a></i>
+                </div>
+
+            </div>
+
+            <hr style="background-color: aliceblue;">
+
+            <div class="copyright">
+                <h6>Copyright <i class="far fa-copyright" aria-hidden="true"> <?php echo date("Y");?></i> Abainemu Enterprises Limited.</h6>   
+            </div>
+
+        </div>
+
+    </div>
+
+
 
 </body>
 </html>
